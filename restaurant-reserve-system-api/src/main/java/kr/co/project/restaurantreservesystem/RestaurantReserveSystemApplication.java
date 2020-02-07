ca16c3a1618c8646c0079e2fb3734926d40118d1
shkim4738 <shkim4738@gmail.com>
2020-02-08 01:03:03 +0900

    init commit - Hello World!

---
+++ restaurant-reserve-system-api/src/main/java/kr/co/project/restaurantreservesystem/RestaurantReserveSystemApplication.java
@@ -0,0 +1,13 @@
+package kr.co.project.restaurantreservesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantReserveSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantReserveSystemApplication.class, args);
+	}
+
+}
---
+++ restaurant-reserve-system-api/src/test/java/kr/co/project/restaurantreservesystem/RestaurantReserveSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.project.restaurantreservesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantReserveSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
