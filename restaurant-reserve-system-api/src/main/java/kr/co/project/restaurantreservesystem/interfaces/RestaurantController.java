356f67898e8b9a06608f4b967b9ef338030eff95
shkim4738 <shkim4738@gmail.com>
2020-02-10 20:36:53 +0900

    가게 상세 - if문 사용

--- restaurant-reserve-system-api/src/main/java/kr/co/project/restaurantreservesystem/interfaces/RestaurantController.java
+++ restaurant-reserve-system-api/src/main/java/kr/co/project/restaurantreservesystem/interfaces/RestaurantController.java
@@ -2,6 +2,7 @@ package kr.co.project.restaurantreservesystem.interfaces;
 
 import kr.co.project.restaurantreservesystem.domain.Restaurant;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -17,5 +18,20 @@ public class RestaurantController {
         Restaurant restaurant = new Restaurant(1004L,"Bob zip", "Seoul");
         restaurants.add(restaurant);
         return restaurants;
+    }
+
+    @GetMapping("/restaurants/{id}")
+    public Restaurant detail(@PathVariable("id") Long id) {
+        Restaurant restaurant = null;
+
+        if (id == 1004L) {
+            restaurant = new Restaurant(1004L,"Bob zip", "Seoul");
+        }
+
+        if (id == 2020L) {
+            restaurant = new Restaurant(2020L,"Cyber Food", "Seoul");
+        }
+
+        return restaurant;
     }
 }
