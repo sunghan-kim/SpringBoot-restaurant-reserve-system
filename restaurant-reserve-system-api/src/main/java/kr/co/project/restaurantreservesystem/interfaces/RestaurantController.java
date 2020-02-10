d0821996de8368694183f5045f48d616064026f8
shkim4738 <shkim4738@gmail.com>
2020-02-10 20:59:20 +0900

    가게 상세 - Repository를 통한 중복된 코드 및 Collection 관리

--- restaurant-reserve-system-api/src/main/java/kr/co/project/restaurantreservesystem/interfaces/RestaurantController.java
+++ restaurant-reserve-system-api/src/main/java/kr/co/project/restaurantreservesystem/interfaces/RestaurantController.java
@@ -1,38 +1,31 @@
 package kr.co.project.restaurantreservesystem.interfaces;
 
 import kr.co.project.restaurantreservesystem.domain.Restaurant;
+import kr.co.project.restaurantreservesystem.domain.RestaurantRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
+
+/*
+    UI Layer는 사용자와 도메인(또는 비즈니스 로직)들이 서로 상관없도록(징검다리 역할만 하도록) 코드를 구현한다.
+    실제로 활용하는 부분은 도메인에서 이루어지도록 한다.
+    즉, Controller는 매우 단순하게 구현하고, 데이터를 가지고 처리하는 부분은 도메인에서 이뤄지도록 한다.
+ */
 
 @RestController
 public class RestaurantController {
 
+    private RestaurantRepository repository = new RestaurantRepository();
+
     @GetMapping("/restaurants")
     public List<Restaurant> list() {
-        List<Restaurant> restaurants = new ArrayList<>();
-
-        Restaurant restaurant = new Restaurant(1004L,"Bob zip", "Seoul");
-        restaurants.add(restaurant);
-        return restaurants;
+        return repository.findAll();
     }
 
     @GetMapping("/restaurants/{id}")
     public Restaurant detail(@PathVariable("id") Long id) {
-
-        List<Restaurant> restaurants = new ArrayList<>();
-        restaurants.add(new Restaurant(1004L,"Bob zip", "Seoul"));
-        restaurants.add(new Restaurant(2020L,"Cyber Food", "Seoul"));
-
-        Restaurant restaurant = restaurants.stream()
-                .filter(r -> r.getId().equals(id))
-                .findFirst()
-                .orElse(null); //.get();
-
-
-        return restaurant;
+        return repository.findById(id);
     }
 }
