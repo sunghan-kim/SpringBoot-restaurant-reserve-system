f465d89e4a33e9e4524f091224bb73e716b74219
shkim4738 <shkim4738@gmail.com>
2020-02-19 18:31:28 +0900

    프로젝트 분리 - Customer 불필요한 기능 제거

--- restaurant-reserve-system-customer-api/src/main/java/kr/co/project/restaurantreservesystem/interfaces/RestaurantController.java
+++ restaurant-reserve-system-customer-api/src/main/java/kr/co/project/restaurantreservesystem/interfaces/RestaurantController.java
@@ -3,12 +3,11 @@ package kr.co.project.restaurantreservesystem.interfaces;
 import kr.co.project.restaurantreservesystem.application.RestaurantService;
 import kr.co.project.restaurantreservesystem.domain.Restaurant;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.util.List;
 
 @CrossOrigin
@@ -28,28 +27,5 @@ public class RestaurantController {
     public Restaurant detail(@PathVariable("id") Long id) {
         Restaurant restaurant = restaurantService.getRestaurant(id); // 기본 정보 + 메뉴 정보
         return restaurant;
-    }
-
-    @PostMapping("/restaurants")
-    public ResponseEntity<?> create(@Valid @RequestBody Restaurant resource) throws URISyntaxException {
-
-        Restaurant restaurant = restaurantService.addRestaurant(
-                Restaurant.builder()
-                    .name(resource.getName())
-                    .address(resource.getAddress())
-                    .build());
-
-        URI location = new URI("/restaurants/" + restaurant.getId());
-        return ResponseEntity.created(location).body("{}");
-    }
-
-    @PatchMapping("/restaurants/{id}")
-    public String update(@PathVariable("id") Long id,
-                         @Valid @RequestBody Restaurant resource) {
-        String name = resource.getName();
-        String address = resource.getAddress();
-        restaurantService.updateRestaurant(id, name, address);
-
-        return "{}";
     }
 }
